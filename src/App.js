522823adb7026d03dc1ecbf9ebcb7e7a73b2c247
宮本隆史 <elephantstone336@gmail.com>
2020-12-09 20:03:43 +0900

    my first React hook with useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const App = () => {
+  const [ count, setCount ] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+  const reset = () => setCount(0)
+  const double = () => setCount(previousCount => previousCount * 2)
+  const divide3 = () => setCount(previousCount => {
+    return previousCount % 3 === 0 ? previousCount / 3 : previousCount
+  })
+
   return (
-    <div>
-      this is a template.
-    </div>
+    <>
+      <div>count: {count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={increment2}>+1</button>
+      <button onClick={decrement2}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={double}>double</button>
+      <button onClick={divide3}>３の倍数は３で割る</button>
+    </>
   );
 }
 
